12da6573eb6d45c1d2851f9a1a1abe11368f452a
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-07-29 22:28:05 +0900

    awesome :lipstick:

--- cookbook/fluentd/recipe.rb
+++ cookbook/fluentd/recipe.rb
@@ -1,15 +1,16 @@
 if node[:fluentd]
   config = node[:fluentd][:config]
-  type = node[:fluentd][:type] || "td-agent"
+  type = node[:fluentd][:type] || 'td-agent'
 
-  directory "/etc/#{type}/config.d"
+  config_dir = "/etc/#{type}/config.d"
+  directory config_dir
 
-  remote_file "/etc/#{type}/td-agent.conf" do
+  remote_file "/etc/#{type}/#{type}.conf" do
     source 'conf/fluentd.conf'
   end
 
   config.each do |conf|
-    remote_file "/etc/td-agent/config.d/#{conf}.conf" do
+    remote_file "/etc/#{type}/config.d/#{conf}.conf" do
       source "conf/#{conf}.conf"
     end
   end
