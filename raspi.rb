05db33d9b1ab679c29cb0f8b4582e766eeec75b4
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-08-14 20:32:22 +0900

    add fluent-bit install

--- raspi.rb
+++ raspi.rb
@@ -2,3 +2,16 @@ include_recipe 'cookbook/update/recipe.rb'
 node[:packages].each do |package|
   package package
 end
+
+node[:git].each do |url|
+  path = url.split('/').last.sub(/\.git$/, '')
+
+  git "/home/ubuntu/#{path}" do
+    repository url
+  end
+
+  execute "build git" do
+    command 'cd /home/ubuntu/fluent-bit/build && cmake .. && make && make install'
+    not_if 'test -e /usr/local/fluent-bit'
+  end
+end
