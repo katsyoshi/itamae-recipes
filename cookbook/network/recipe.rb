e5c2b31b717d70e5ca2cea3b60e408b62ef91bb0
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2017-04-08 17:23:32 +0900

    set hostname for hosts file and hostname file

--- cookbook/network/recipe.rb
+++ cookbook/network/recipe.rb
@@ -1,5 +1,21 @@
-template '/etc/network/interfaces' do
+template "/etc/network/interfaces" do
   action :create
   source "templates/etc/network/interfaces.erb"
   variables(networks: node.networks)
 end
+
+hostname = node&.hostname
+hostname&.tap do |hn|
+  template "/etc/hostname" do
+    action :create
+    source  "templates/etc/hostname.erb"
+    variables(hostname: hn)
+  end
+
+  template "/etc/hosts" do
+    action :create
+    source "templates/etc/hosts.erb"
+    variables(hostname: hn)
+  end
+end
+
