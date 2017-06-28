abbd1e8c387ef90225ac75bfb7049bd4ee7bf853
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2017-06-29 01:16:55 +0900

    setup emacs

--- cookbook/emacs/recipe.rb
+++ cookbook/emacs/recipe.rb
@@ -1,14 +1,27 @@
-home = node.emacs.config_dir
-font = node.emacs.font
+emacs = node.emacs
+home = emacs.config_dir || File.expand_path("~/.") + "/.emacs.d"
+font = emacs.font
+packages = emacs.packages
 
-git "~/.gitsrc/dot.emacs.d" do
+git "#{home}" do
   action :sync
-  repogitory "git@github.com:katsoyshi/dot.emacs.d"
-  destination "#{home}/.emacs.d"
+  repository "https://github.com/katsyoshi/dot.emacs.d"
+  not_if "test -d #{home}"
 end
 
 template "#{home}/settings/font.el" do
   action :create
   source "templates/dot.emacs.d/settings/font.el.erb"
-  valiables(font: font.family, size: font.size, os: node.platform)
+  variables(family: font.family, height: font.height, os: node.platform)
+end
+
+template "#{home}/settings/package.el" do
+  action :create
+  source "templates/dot.emacs.d/settings/package.el.erb"
+end
+
+template "#{home}/init.el" do
+  action :create
+  source "templates/dot.emacs.d/settings/init.el.erb"
+  variables(home: home, packages: packages)
 end
