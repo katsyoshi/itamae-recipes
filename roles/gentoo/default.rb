7f3d7aa9c4d598ae1e8ba3878529b49d104d9493
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2017-08-13 22:39:18 +0900

    use method

--- roles/gentoo/default.rb
+++ roles/gentoo/default.rb
@@ -1,2 +1,2 @@
-include_recipe "#{__dir__}/../../cookbooks/gentoo/recipe" if node[:packages]
-include_recipe "#{__dir__}/../../cookbooks/emacs/recipe" if node[:emacs]
+include_recipe "#{__dir__}/../../cookbooks/gentoo/recipe" if node&.packages
+include_recipe "#{__dir__}/../../cookbooks/emacs/recipe" if node&.emacs
