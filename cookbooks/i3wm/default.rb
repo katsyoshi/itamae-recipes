c3eba805e419c7244e69c1cd991ffb4800177a63
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2019-02-06 09:35:27 +0900

    use templates

--- cookbooks/i3wm/default.rb
+++ cookbooks/i3wm/default.rb
@@ -3,6 +3,6 @@ home = i3wm.config_dir || Fil.join(File.expand_path("~/"), "/.config", "i3")
 
 template "#{home}/config" do
   action :create
-  source "templates/i3wm/config"
+  source "templates/i3wm/config.erb"
   variables(i3wm: i3wm)
 end
